35d042406aff80161f6e0eea0dd5f72bcae96318
Arnau Gómez Farell <agomez1f@gmail.com>
2021-07-19 08:29:33 +0200

    design background

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreenBackground extends StatelessWidget {
@@ -14,7 +16,32 @@ class HomeScreenBackground extends StatelessWidget {
     return Stack(
       children: [
         Container(decoration: boxDecoration),
+        Positioned(top: -100, left: -30, child: _PinkBox()),
       ],
+    );
+  }
+}
+
+class _PinkBox extends StatelessWidget {
+  const _PinkBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -pi / 5,
+      child: Container(
+        width: 360,
+        height: 360,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(80),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(236, 98, 188, 1),
+            Color.fromRGBO(241, 142, 172, 1),
+          ]),
+        ),
+      ),
     );
   }
 }
